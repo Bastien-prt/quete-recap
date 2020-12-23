bdd1ad2b17bf56a1898d9df9104e9ea0cfa58796
Bastien Perrot <bastien.perrot1@gmail.com>
2020-12-23 11:33:09 +0100

    perrot_bastien_quete020

--- front/odysseyhomer/src/components/SignUp.jsx
+++ front/odysseyhomer/src/components/SignUp.jsx
@@ -1,23 +1,95 @@
 import React, { useState } from 'react';
 
+import "../styles/SignUp.css"
+
 function SignUp(){
     const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirmation, setPasswordConfirmation] = useState('')
+    const [name, setName] = useState('')
+    const [lastname, setLastname] = useState('')
+
+    const data = {
+        email,
+        password,
+        passwordConfirmation,
+        name,
+        lastname
+    }
+
+    const dataString= JSON.stringify(data)
+
+
 
     const updateEmailField = (e) =>{
         setEmail(e.target.value)
     }
+    const updatePasswordField = (e) =>{
+        setPassword(e.target.value)
+    }
+    const updatePasswordConfirmationField = (e) =>{
+        setPasswordConfirmation(e.target.value)
+    }
+    const updateNameField = (e) =>{
+        setName(e.target.value)
+    }
+    const updateLastnameField = (e) =>{
+        setLastname(e.target.value)
+    }
 
 
-    return (
+    const handleSubmit = (e) => {
+        console.log(data)
+        e.preventDefault();
+    }
+
+   return (
         <div>
-            <h1> Your email : {email} </h1>
-            <input 
-                type="email" 
-                name="email" 
-                id="email"
-                value={email}
-                onChange={updateEmailField}
-                placeholder="Your email"/>
+            <h1> Your informations : <div className="data">{dataString} </div> </h1>
+            <form action="">
+                <input 
+                    type="email" 
+                    name="email" 
+                    id="email"
+                    value={email}
+                    onChange={updateEmailField}
+                    placeholder="Your email"
+                />
+                <input 
+                    type="password" 
+                    name="paswword" 
+                    id="password"
+                    value={password}
+                    onChange={updatePasswordField}
+                    placeholder="Your password"
+                />
+                <input 
+                    type="password-confirmation" 
+                    name="paswword-confirmation" 
+                    id="password-confirmation"
+                    value={passwordConfirmation}
+                    onChange={updatePasswordConfirmationField}
+                    placeholder="Your password"
+                />
+                <input 
+                    type="name" 
+                    name="name" 
+                    id="name"
+                    value={name}
+                    onChange={updateNameField}
+                    placeholder="name"
+                />
+                <input 
+                    type="lastname" 
+                    name="lastname" 
+                    id="lastname"
+                    value={lastname}
+                    onChange={updateLastnameField}
+                    placeholder="lastname"
+                />
+                <input type="submit" value="Soumettre" onClick={handleSubmit}/>
+            </form>
+            
                 
         </div>
 
