50f8c75e648dc972871734a3af1beb4df14e0da7
Bastien Perrot <bastien.perrot1@gmail.com>
2021-01-04 21:33:40 +0100

    quete060

--- front/odysseyhomer/src/App.js
+++ front/odysseyhomer/src/App.js
@@ -2,11 +2,48 @@ import React from 'react';
 
 import SignUp from  "./components/SignUp";
 
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 function App() {
   return (
     <div className="App">
-     <SignUp />
+      <MuiThemeProvider>
+        <Grid  container
+          alignItems='center'
+          style={{ height:  '100%' }}>
+             <Grid  item  xs={12} sm={12} style={{ "text-align": "center" }}>
+               <Paper
+                elevation={4}
+                style={{ margin:  32 }}
+                >
+                  <Grid  container
+                    alignItems='center'
+                    justify='center'
+                  >
+
+                     
+                     <Grid item xs={12} sm={6} 
+                        style={{ 'text-align': 'center' }}> 
+                          <img src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png" /> 
+                     </Grid> 
+                     <Grid
+                          item
+                          xs={12}
+                          sm={6}
+                          direction='column'
+                          alignItems='center'>
+                          <SignUp  />
+                      </Grid>
+
+                    </Grid>
+                  
+               </Paper>
+             </Grid>
+        </Grid>
+      </MuiThemeProvider>
+     
     </div>
   );
 }
